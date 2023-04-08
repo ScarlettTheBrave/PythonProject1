2c7b3943d19b30e5b5eee91b01fcb166e5ad6ae8
ScarlettTheBrave <artemmarikov49@gmail.com>
2023-04-08 10:34:09 +0300

    lab 2

---
+++ lab_2_MarikovArtem.py
@@ -0,0 +1,23 @@
+print("Programming", "Essential", "in", sep='*' + '*' + '*', end = "...")
+print("Python")
+
+
+print("\t\t*")
+print("\t  *", "  *", sep=' ')
+print("\t *", " *", sep='    ')
+print("\t*", "  *", sep='     ')
+print("   ***", "\t   ***", sep=' ')
+print("\t*", " *", sep='      ')
+print("\t*", " *", sep='      ')
+print("  *"," *"," *"," *"," *"," *"," *", sep='')
+
+print("\"i \'m\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
+
+number = "500"
+octal_number = int(number, 8)
+print(octal_number)
+
+
+num = "777"
+decimal_num = int(num, 16)
+print(decimal_num)
