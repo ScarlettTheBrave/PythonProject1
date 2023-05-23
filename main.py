41dc8bb3882ae6d7f13e124570c12ba6add70c58
ScarlettTheBrave <artemmarikov49@gmail.com>
2023-05-23 09:35:29 +0300

    lab9

---
+++ main.py
@@ -0,0 +1,13 @@
+from sys import path
+path.append('..\\module’)
+
+import module
+
+print(module.counter)
+
+from module import suml, prodl
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(suml(zeroes))
+print(prodl(ones))
